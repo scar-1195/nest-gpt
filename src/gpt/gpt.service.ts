e04a35d5a85f0e617d48b2329a2b369f9551fab8
oscar lopez <o.lrodriguez1195@gmail.com>
2025-01-25 21:54:47 -0600

    feat(pros_cons):use_case_done

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
-import { orthographyUseCase } from './use-cases';
-import { OrthographyDto } from './dtos';
+import {
+  orthographyUseCase,
+  prosConsDiscusserStreamUseCase,
+  prosConsDiscusserUseCase,
+} from './use-cases';
+import { OrthographyDto, ProsConsDiscusserDto } from './dtos';
 
 @Injectable()
 export class GptService {
@@ -15,5 +19,12 @@ export class GptService {
     return await orthographyUseCase(this.openai, {
       prompt: orthographyDto.prompt,
     });
+  }
+
+  async prosConsDiscusser({ prompt }: ProsConsDiscusserDto) {
+    return await prosConsDiscusserUseCase(this.openai, { prompt });
+  }
+  async prosConsDiscusserStream({ prompt }: ProsConsDiscusserDto) {
+    return await prosConsDiscusserStreamUseCase(this.openai, { prompt });
   }
 }
