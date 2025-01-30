22fe91f66fc52ba630d9631e1b105cdf9de0aace
oscar lopez <o.lrodriguez1195@gmail.com>
2025-01-29 18:33:31 -0600

    feat(assistant):create_assistant_openia

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GptModule } from './gpt/gpt.module';
+import { SamAssistantModule } from './sam-assistant/sam-assistant.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GptModule],
+  imports: [ConfigModule.forRoot(), GptModule, SamAssistantModule],
   controllers: [],
   providers: [],
 })
